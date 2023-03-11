1f44de4b4eb4f1a717ddfb9e13730c923ae66b57
Denloob <magtop@denloob.tk>
2023-03-11 13:08:51 +0200

    Add wifi icon script

---
+++ scripts/wifi.sh
@@ -0,0 +1,27 @@
+INTERFACE=wlan0
+
+# when no wireless at all, do not display this block
+if [ ! -d "/sys/class/net/${INTERFACE}/wireless" ]; then
+    exit
+fi
+
+# if wifi down
+if [ "$(cat /sys/class/net/$INTERFACE/operstate)" = "down" ]; then
+    printf "󰤮"
+    exit
+fi
+
+QUALITY=$(grep $INTERFACE /proc/net/wireless | awk '{ print int($3 * 100 / 70) }')
+
+if (( $QUALITY > 85 )); then
+    printf "󰤨"
+elif (( $QUALITY > 70 )); then
+    printf "󰤥"
+elif (( $QUALITY > 50 )); then
+    printf "󰤢"
+elif (( $QUALITY > 30 )); then
+    printf "󰤟"
+else 
+    printf "󰤯"
+fi 
+
