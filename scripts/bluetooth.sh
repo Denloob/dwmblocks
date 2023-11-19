2c6f71732280bc5822871c985163551a5550c61e
Denloob <den@denloob.uk>
2023-11-19 11:34:00 +0200

    Add toggle bluetooth

--- scripts/bluetooth.sh
+++ scripts/bluetooth.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-ISRUNNING=$(systemctl status bluetooth.service | grep "Active: active")
-
 case $BUTTON in
     1) kitty -e bluetuith ;;
     4) kitty -e "$EDITOR" "$0" ;;
+    6) bluetooth toggle
 esac
+
+ISRUNNING=$(bluetooth | grep "on")
 
 if [ -n "$ISRUNNING" ]; then
     printf ""
